2d9a24a01c78faecf834835abc1b083ddce8333c
Nandani Srivastava <srivastavanandani190@gmail.com>
2026-02-01 19:07:52 +0530

    Initial commit: Java programs

---
+++ BASIC CORE/basic.java
@@ -0,0 +1,13 @@
+public class basic {
+
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello, Nandani!");
+        System.out.println("Welcome to Java programming.");
+        //addition
+        System.out.println("The sum of 5 and 10 is: " + (5 + 10));
+        int num=25;
+        int square=num*num;
+        System.out.println("The square of " + num + " is: " + square); 
+    }
+}
