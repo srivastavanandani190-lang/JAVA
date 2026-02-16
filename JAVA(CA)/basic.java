2ff36823cfe766672255e1ed539d70552b45780c
Nandani Srivastava <srivastavanandani190@gmail.com>
2026-02-16 20:17:10 +0530

    basic

---
+++ JAVA(CA)/basic.java
@@ -0,0 +1,11 @@
+//java is programming language which is portable,simple,object oriented and secure.
+//java is platform independent language because of byte code and jvm.
+//java is used for web application,desktop application,mobile application,enterprise application etc.
+//java is developed by james gosling in 1995.
+//java is owned by oracle corporation.
+//JVM(java virtual machine) is a software that runs java byte code and provides platform independence.
+public class basic{
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
