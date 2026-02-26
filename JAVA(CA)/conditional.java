b7abeba8d0919d4d221526f1bfe8a9fc13405bbd
Nandani Srivastava <srivastavanandani190@gmail.com>
2026-02-26 20:24:19 +0530

    selection statement

---
+++ JAVA(CA)/conditional.java
@@ -0,0 +1,19 @@
+class conditional
+{
+    public static void main(String args[])
+    {
+        //flow of control statements
+        //selection statements
+        //if statement
+        int a=10,b=20;
+        if(a>b)
+        {
+            System.out.println("a is greater than b");
+        }
+        else
+        {
+            System.out.println("b is greater than a");
+        }
+        
+    }
+}
