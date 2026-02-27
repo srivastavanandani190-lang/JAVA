46ebaab0a6b46c89594bf3aa0418f167dd475835
Nandani Srivastava <srivastavanandani190@gmail.com>
2026-02-27 12:55:07 +0530

    leap year

---
+++ JAVA(CA)/SELECTIONSTATEMENT/leapyear.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class leapyear
+{
+    public static void main(String args[])
+    {
+        int year;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the year:");
+        year=sc.nextInt();
+        if((year%4==0 && year%100!=0) || (year%400==0))
+        {
+            System.out.println(year+" is a leap year.");
+        }
+        else
+        {
+            System.out.println(year+" is not a leap year.");
+        }
+        sc.close();
+    }
+}
