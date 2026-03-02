5bed8df118840b5db20b5721966eb235539f41e7
Nandani Srivastava <srivastavanandani190@gmail.com>
2026-03-02 18:36:40 +0530

    string

---
+++ JAVA(CA)/STRING/BASIC.java
@@ -0,0 +1,9 @@
+class BASIC{
+    public static void main(String[] args) {
+        String name="Nandani Srivastava";
+        System.out.println(name);
+        String info="student of Computer Science and Engineering";
+        System.out.println(name+" "+info);
+
+    }
+}
