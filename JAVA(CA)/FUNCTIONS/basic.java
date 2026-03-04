00a89bccc859e5684ae61579e44132c2e7182df8
Nandani Srivastava <srivastavanandani190@gmail.com>
2026-03-04 12:21:11 +0530

    functions

--- JAVA(CA)/FUNCTIONS/basic.java
+++ JAVA(CA)/FUNCTIONS/basic.java
@@ -1,20 +1,36 @@
-class add{
-    int a,b;
-    add(int x,int y){
-        a=x;
-        b=y;
-    }
-    void sum(){
-        System.out.println("The sum is "+(a+b));
-    }
-}
 public class basic {
     public static void main(String[] args) {
-        add obj=new add(10,20);
-        obj.sum();
+        int result = sum(10,20);
+        System.out.println("The sum is: " + result);
+        greet();
+        int randomNumber = getRandomNumber();
+        System.out.println("Random number: " + randomNumber);
+        printSum(5, 7);
+
+    }
+
+    //no input and no output function
+static void greet(){
+    System.out.println("hello world");
+    //return ; optional in void function
+}
+
+//no input and some output function
+static int getRandomNumber(){
+    return (int)(Math.random()*100);
+}
+
+//some input and no output function
+static void printSum(int a,int b){//Number of parameters can be zero or more than zero
+    int c=a+b;
+    System.out.println("the sum is "+c);
+}
+
+//some input and some output function
+ static int sum(int a,int b){
+        int c=a+b;
+        return c;
     }
 }
 // fucntions are used to perform a specific task and can be called multiple times in a program.  
 // functions increases the reusability of code and make it more organized. 
-// We can create multiple objects of the class "add" and call the sum() function
-// to perform the addition operation with different values of a and b.
