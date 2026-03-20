9e3ab0c320f5ef46956e224b695e76f7b1ab4215
Nandani Srivastava <srivastavanandani190@gmail.com>
2026-03-20 19:33:11 +0530

    final

---
+++ JAVA(CA)/OOPS/final1.java
@@ -0,0 +1,12 @@
+class Random{
+    final double pi;
+    Random(){
+        pi = 3.14;
+    }
+}
+public class final1 {
+    public static void main(String[] args) {
+        Random r = new Random();
+        System.out.println(r.pi);
+    }
+}
