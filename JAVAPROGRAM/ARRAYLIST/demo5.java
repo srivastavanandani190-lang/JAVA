eb33b779c0261e7a1680f22062b1c2184c73c43d
Nandani Srivastava <srivastavanandani190@gmail.com>
2026-05-15 22:28:12 +0530

    linkedlist

--- JAVAPROGRAM/ARRAYLIST/demo5.java
+++ JAVAPROGRAM/ARRAYLIST/demo5.java
@@ -6,16 +6,18 @@ public class demo5{
          list.add("World");
             list.add("Java");
             System.out.println("LinkedList: " + list);
-             list.remove("World");
-             System.out.println("After removing 'World': " + list);
-              list.addFirst("Welcome");
-              System.out.println("After adding 'Welcome' at the beginning: " + list);
-               list.addLast("Programming");
-               System.out.println("After adding 'Programming' at the end: " + list);
-                String firstElement = list.getFirst();
-                String lastElement = list.getLast();
-                System.out.println("First element: " + firstElement);
-                System.out.println("Last element: " + lastElement);
-                
+                list.remove("World");
+                System.out.println("After removing 'World': " + list);
+               list.addFirst("Welcome");
+               System.out.println("After adding 'Welcome' at the beginning: " + list);  
+                list.addLast("Programming");
+                System.out.println("After adding 'Programming' at the end: " + list);
+                 String firstElement = list.getFirst();
+                 System.out.println("First element: " + firstElement);
+                  String lastElement = list.getLast();
+                  System.out.println("Last element: " + lastElement);
+                     list.clear();
+                     System.out.println("After clearing the LinkedList: " + list);
+                        
     }
 }
